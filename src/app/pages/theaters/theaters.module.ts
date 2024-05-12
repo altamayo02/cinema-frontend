f1c79f025dda25df12ae9b723db7f6a94b296d25
altamayo02 <korkonica@gmail.com>
2024-05-12 14:24:11 -0500

    Monday classwork

--- src/app/pages/theaters/theaters.module.ts
+++ src/app/pages/theaters/theaters.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { ManageComponent } from './manage/manage.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TheatersRoutingModule } from './theaters-routing.module';
 
 
 
@@ -11,7 +13,10 @@ import { ManageComponent } from './manage/manage.component';
     ManageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TheatersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TheatersModule { }
